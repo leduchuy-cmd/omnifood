6d356dfed566a370baf10aaec2073abfeae60c90
HUY LE DUC <huyle010998@gmail.com>
2022-05-11 22:51:32 +0700

    upload code

---
+++ js/script.js
@@ -0,0 +1,134 @@
+$(document).ready(function () {
+  /* FOR THE STICKY*/
+  $(".js--section-features").waypoint(
+    function (direction) {
+      if (direction == "down") {
+        $("nav").addClass("sticky");
+      } else {
+        $("nav").removeClass("sticky");
+      }
+    },
+    { offset: "60px;" }
+  );
+
+  /* Scroll on buttons */
+
+  $(".js--scroll-to-plans").click(function () {
+    $("html, body").animate(
+      { scrollTop: $(".js--section-plans").offset().top },
+      1000
+    );
+  });
+
+  //
+  $(".js--scroll-to-start").click(function () {
+    $("html, body").animate(
+      { scrollTop: $(".js--section-features").offset().top },
+      1000
+    );
+  });
+
+  /* Navigation scroll */
+  // Select all links with hashes
+  $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function (event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length
+          ? target
+          : $("[name=" + this.hash.slice(1) + "]");
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $("html, body").animate(
+            {
+              scrollTop: target.offset().top,
+            },
+            1000,
+            function () {
+              // Callback after animation
+              // Must change focus!
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(":focus")) {
+                // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+              }
+            }
+          );
+        }
+      }
+    });
+
+  /*Animations on scroll */
+  $(".js--wp-1").waypoint(
+    function (direction) {
+      $(".js--wp-1").addClass("animated fadeIn");
+    },
+    {
+      offset: "50%",
+    }
+  );
+
+  $(".js--wp-2").waypoint(
+    function (direction) {
+      $(".js--wp-2").addClass("animated fadeInUp");
+    },
+    {
+      offset: "50%",
+    }
+  );
+
+  $(".js--wp-3").waypoint(
+    function (direction) {
+      $(".js--wp-3").addClass("animated fadeInUp");
+    },
+    {
+      offset: "50%",
+    }
+  );
+
+  $(".js--wp-4").waypoint(
+    function (direction) {
+      $(".js--wp-3").addClass("animated fadeIn");
+    },
+    {
+      offset: "50%",
+    }
+  );
+
+  // Mobile nav
+  $(".js--nav-icon").click(function () {
+    var nav = $(".js--main-nav");
+    var icon = $(".js--nav-icon i");
+
+    nav.slideToggle(200);
+
+    if (icon.hasClass("bar-icon")) {
+      icon.addClass("close-icon");
+      icon.removeClass("bar-icon");
+    } else {
+      icon.addClass("fa fa-bars");
+      // icon.removeClass("fa fa-times");
+    }
+  });
+});
+const js_nav_icon = document.querySelector(".js--nav-icon");
+const bar = document.querySelector(".bar-icon");
+const close = document.querySelector(".close-icon");
+const main_nav = document.querySelector(".js--main-nav");
+
+// Event
